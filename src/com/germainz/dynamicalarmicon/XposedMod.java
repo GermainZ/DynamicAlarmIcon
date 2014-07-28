2125477fc5c3a63518169a03f5320360766cd5d8
GermainZ <germanosz@gmail.com>
2014-07-28 20:22:41 +0300

    Add Timely support

--- src/com/germainz/dynamicalarmicon/XposedMod.java
+++ src/com/germainz/dynamicalarmicon/XposedMod.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.provider.Settings;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -24,12 +27,17 @@ public class XposedMod implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 
-        if (!lpparam.packageName.equals("com.android.systemui"))
-            return;
+        if (lpparam.packageName.equals("com.android.systemui"))
+            hookSystemUI(lpparam.classLoader);
+        else if (lpparam.packageName.equals("ch.bitspin.timely"))
+            hookTimely(lpparam.classLoader);
 
-        findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBarPolicy",
-                lpparam.classLoader, "updateAlarm", Intent.class,
-                new XC_MethodReplacement() {
+
+    }
+
+    private void hookSystemUI(ClassLoader classLoader) {
+        findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBarPolicy", classLoader, "updateAlarm",
+                Intent.class, new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
                         boolean alarmSet = ((Intent) param.args[0]).getBooleanExtra("alarmSet", false);
@@ -54,8 +62,7 @@ public class XposedMod implements IXposedHookLoadPackage {
                 }
         );
 
-        findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar",
-                lpparam.classLoader, "makeStatusBarView",
+        findAndHookMethod("com.android.systemui.statusbar.phone.PhoneStatusBar", classLoader, "makeStatusBarView",
                 new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
@@ -83,6 +90,35 @@ public class XposedMod implements IXposedHookLoadPackage {
                         intentFilter.addAction(UPDATE_ALARM_ICON);
                         Context context = (Context) getObjectField(param.thisObject, "mContext");
                         context.registerReceiver(broadcastReceiver, intentFilter);
+                    }
+                }
+        );
+    }
+
+    private void hookTimely(final ClassLoader classLoader) {
+        findAndHookMethod("ch.bitspin.timely.alarm.AlarmManager", classLoader, "f", "ch.bitspin.timely.data.AlarmClock",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        // Timely actually does this for Android 4.1 and less (but not for 4.2+) but only after the
+                        // ALARM_CHANGED intent is sent, which is too late.
+                        String nextAlarmFormatted = "";
+                        Context context = (Context) getObjectField(param.thisObject, "e");
+                        Object alarmClock = param.args[0];
+                        if (alarmClock != null) {
+                            Object nextAlarmUTC = callStaticMethod(findClass("ch.bitspin.timely.alarm.e", classLoader),
+                                    "a", alarmClock);
+                            if (nextAlarmUTC != null) {
+                                Object nextAlarmLocal = callMethod(nextAlarmUTC, "c", (Object) null);
+                                String timeFormat = "E kk:mm";
+                                if (!DateFormat.is24HourFormat(context))
+                                    timeFormat = "E h:mm aa";
+                                nextAlarmFormatted = (String) DateFormat.format(timeFormat,
+                                        (Long) callMethod(nextAlarmLocal, "d"));
+                            }
+                        }
+                        Settings.System.putString(context.getContentResolver(),
+                                Settings.System.NEXT_ALARM_FORMATTED, nextAlarmFormatted);
                     }
                 }
         );
